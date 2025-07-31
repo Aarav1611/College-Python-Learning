83cea2afe9ee11f82946d0eb4988b76bee829ac3
Aarav1611 <aaravsaroliya@gmail.com>
2025-07-31 13:19:52 -0700

    Chapter-3

--- Chapter-3/01_intro_to_strings.py
+++ Chapter-3/01_intro_to_strings.py
@@ -1,1 +1,8 @@
-print("Hello world")
+name="Aarav"
+
+nameshort= name[0:3] # start from index 0 all way till 3 (excluding 3)
+print(nameshort)
+print(name[0:4])
+
+character1= name[2]
+print(character1)
---
+++ Chapter-3/03_str_functions.py
@@ -0,0 +1,7 @@
+name="aarav"
+
+print(len(name))
+print(name.endswith("rav"))
+print(name.startswith("Aar"))
+print(name.capitalize())
+
